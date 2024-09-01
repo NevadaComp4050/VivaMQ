16e030205f0fcab469b02e1d4b65c4785fc55aa2
AaronMQ <achakerian@gmail.com>
2024-09-01 19:28:50 +1000

    docs: usage guide for BE

--- ai-processor/src/beSkel.ts
+++ ai-processor/src/beSkel.ts
@@ -13,6 +13,7 @@ import { pdfToText } from "./extractPDF";
         await channel.assertQueue(receiveQueue, { durable: false });
         await channel.assertQueue(sendQueue, { durable: false });
     
+        // s represents the student submission
         const s = `
 Nuclear physics is the study of atomic nuclei, their components, and the forces that govern their interactions. This field explores the fundamental forces of nature, particularly the strong nuclear force, which binds protons and neutrons within an atomic nucleus. One fascinating concept in nuclear physics is nuclear binding energy, the energy needed to break a nucleus into its separate protons and neutrons. This energy difference also corresponds to a mass difference, as described by Einstein’s equation, E=mc^2. Understanding these forces and energies helps explain why some atomic nuclei are stable while others undergo radioactive decay.
 
@@ -20,8 +21,9 @@ A key focus in nuclear physics is nuclear reactions, such as fission and fusion.
 
 Beyond energy, nuclear physics has numerous practical applications. In medicine, radioactive isotopes are used in imaging techniques like PET scans and in radiation therapy to target cancer cells. Radiocarbon dating, a tool in archaeology and geology, relies on the predictable decay of carbon-14 isotopes to determine the age of organic materials, aiding in historical and environmental research. Additionally, nuclear physics has contributed to the development of particle physics, offering insights into the fundamental particles and forces that shape our universe. These diverse applications highlight the profound impact of nuclear physics on both science and everyday life.
 `;
+        const u = "12345"; //represents the UUID
         const pdf = pdfToText('./PDFs/example.pdf')
-        const sendMsg = Buffer.from(JSON.stringify([s,"12345"]));        
+        const sendMsg = Buffer.from(JSON.stringify([s,u]));        
         channel.sendToQueue(sendQueue, sendMsg);
         channel.consume(receiveQueue, async (msg: amqp.ConsumeMessage | null) => {
             if (msg) {

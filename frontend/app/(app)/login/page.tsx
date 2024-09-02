9e3d35d819fe9cbfae0e3f1bfadfe66abc0e3030
Fozzyishere <danielpham13092002@gmail.com>
2024-09-02 21:54:29 +1000

    feat: basic login page design added

--- frontend/app/(app)/login/page.tsx
+++ frontend/app/(app)/login/page.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Link from 'next/link';
+import Link from "next/link";
 import { Button } from "~/components/ui/button";
 import {
   Card,
@@ -13,15 +13,26 @@ import { useSidebarToggle } from "~/hooks/use-sidebar-toggle";
 export default function Login() {
   return (
     <div className="flex items-center justify-center h-screen">
-      <Card className="w-1/4 place-content-center p-10">
+      <Card className="w-auto place-content-center p-10">
         <CardTitle>Login</CardTitle>
         <CardContent className="mt-5">
-          <Input placeholder="MQ ID" className="flex mb-5" />
-          <Input placeholder="Password" className="flex mb-5" />
+          <Input placeholder="OneID" className="flex md:w-96 sm:w-auto mb-5" />
+          <Input
+            placeholder="Password"
+            className="flex md:w-96 sm:w-auto mb-5"
+          />
           <Button className="float-right" variant="default" size="default">
             <Link href={"/dashboard"}>Login</Link>
           </Button>
-          <p className="text-xs opacity-50">Can't log in? <a href="https://students.mq.edu.au/support/technology/service-desk" className="">Contact Help Desk</a></p>
+          <p className="text-xs opacity-50">
+            Can't log in?{" "}
+            <a
+              href="https://students.mq.edu.au/support/technology/service-desk"
+              className=""
+            >
+              Contact Help Desk
+            </a>
+          </p>
         </CardContent>
       </Card>
     </div>

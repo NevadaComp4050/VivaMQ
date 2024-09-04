4b650af9e67857adc3cfd58fa87ead04a9004955
Adrian Kane <egg_on_stilts@hotmail.com>
2024-09-05 01:07:38 +1000

    feat(FileIO): Mostly abstracted file upload and download so that the service layer can be replaced in the future

--- backend/src/modules/index.ts
+++ backend/src/modules/index.ts
@@ -5,9 +5,7 @@ import units from './Unit/Unit.route';
 import tutors from './Tutor/Tutor.route';
 import students from './Student/Student.route';
 import assignments from './Assignment/Assignment.route';
-
 import FileIO from './FileIO/FileIO.route';
-
 import submissions from './Submission/Submission.route';
 
 const router: Router = Router();
@@ -17,8 +15,7 @@ router.use('/units', units);
 router.use('/tutors', tutors);
 router.use('/students', students);
 router.use('/assignments', assignments);
-
-router.use('/files',FileIO);
+router.use('/files', FileIO);
 router.use('/submissions', submissions);
 
 export default router;

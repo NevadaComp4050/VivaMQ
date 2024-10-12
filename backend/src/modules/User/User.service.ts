c09387bfc142f869a24a1e5a724b2fe0ea940f7b
Ryan Kontos <ryanjkontos@gmail.com>
2024-10-13 01:28:35 +1100

    feat: Registering now works from frontend to backend

--- backend/src/modules/User/User.service.ts
+++ backend/src/modules/User/User.service.ts
@@ -4,15 +4,11 @@ import argon2 from 'argon2';
 
 export default class UserService {
 
-
-
 public async createUser(data: Omit<User, 'id'>) {
   const hashedPassword = await argon2.hash(data.password);
 
-  
   console.log(data);
 
- 
   const user = await prisma.user.create({
     data: {
       ...data,
@@ -22,7 +18,6 @@ public async createUser(data: Omit<User, 'id'>) {
   });
   return user;
 }
-
 
   public async validateUser(email: string, password: string) {
     const user = await prisma.user.findUnique({

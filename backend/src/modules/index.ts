1693e8d04587560b9a6b4d807daf251e7d3c9e16
Deb Kumar <59546212+MegaPanchamZ@users.noreply.github.com>
2024-10-23 22:16:40 +1100

    feat: add rubrics route to main router

--- backend/src/modules/index.ts
+++ backend/src/modules/index.ts
@@ -1,14 +1,15 @@
 import { Router } from 'express';
 
-import users from './User/user.route';
+import users from './User/User.route';
 import units from './Unit/Unit.route';
 import tutors from './Tutor/Tutor.route';
 import students from './Student/Student.route';
-import assignments from './Assignment/assignment.route';
+import assignments from './Assignment/Assignment.route';
 import submissions from './Submission/Submission.route';
 import vivaQuestions from './VivaQuestion/VivaQuestion.route';
 
 import misc from './misc.route';
+import rubrics from './Rubric/rubric.route';
 
 const router: Router = Router();
 
@@ -19,7 +20,7 @@ router.use('/students', students);
 router.use('/assignments', assignments);
 router.use('/submissions', submissions);
 router.use('/viva-questions', vivaQuestions);
-
+router.use('/rubrics', rubrics);
 router.use('/misc', misc);
 
 export default router;

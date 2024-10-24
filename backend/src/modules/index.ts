ff1bae02bab3f597cacb45e5320dad07224ad46f
Ryan Kontos <ryanjkontos@gmail.com>
2024-10-24 20:41:16 +1100

    Fixed casing in backend module index

--- backend/src/modules/index.ts
+++ backend/src/modules/index.ts
@@ -4,12 +4,12 @@ import users from './User/User.route';
 import units from './Unit/Unit.route';
 import tutors from './Tutor/Tutor.route';
 import students from './Student/Student.route';
-import assignments from './Assignment/assignment.route';
+import assignments from './Assignment/Assignment.route';
 import submissions from './Submission/Submission.route';
 import vivaQuestions from './VivaQuestion/VivaQuestion.route';
 
 import misc from './misc.route';
-import rubrics from './Rubric/rubric.route';
+import rubrics from './Rubric/Rubric.route';
 
 const router: Router = Router();
 

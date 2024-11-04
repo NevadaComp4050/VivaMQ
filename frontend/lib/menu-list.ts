f36a6b6ba4a61b72d8c4a4e18b7ba3fd6164f9b3
Deb Kumar <59546212+MegaPanchamZ@users.noreply.github.com>
2024-11-04 20:05:35 +1100

    Refactor: update menu list and search data URLs; remove unused tutors page

--- frontend/lib/menu-list.ts
+++ frontend/lib/menu-list.ts
@@ -53,13 +53,6 @@ export function getMenuList(pathname: string): Group[] {
           icon: Bookmark,
           submenus: [],
         },
-        /* {
-          href: " /dashboard/quality-assurance",
-          label: "Quality Assurance",
-          active: pathname.startsWith("/dashboard/quality-assurance"),
-          icon: BookLock,
-          submenus: [],
-        }, */
         {
           href: "/dashboard/rubrics",
           label: "Rubrics",
@@ -76,29 +69,5 @@ export function getMenuList(pathname: string): Group[] {
         },
       ],
     },
-    /* {
-      groupLabel: "Users",
-      menus: [
-        {
-          href: "/dashboard/tutors",
-          label: "Tutors",
-          active: pathname === "/dashboard/tutors",
-          icon: Users,
-          submenus: [],
-        },
-      ],
-    }, */
-    /* {
-      groupLabel: "System",
-      menus: [
-        {
-          href: "/dashboard/settings",
-          label: "Settings",
-          active: pathname === "/dashboard/settings",
-          icon: Settings,
-          submenus: [],
-        },
-      ],
-    }, */
   ];
 }
